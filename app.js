e5973cd53224b6b24cdebe2c5d8aa24720cc4565
Younes-Charguioui <72479723+Younes-Charguioui@users.noreply.github.com>
2023-09-01 22:06:40 +0200

    update modal content
    Ajout de lien vers les projets github dans les modal

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ let tabImages = []
 let index = 0
 
 const bookiTechno = "- HTML<br>- Css<br>- Git<br>- Font Awesome"
-const bookiDescription = "Construction d'un site web d'hébergement avec de simple language HTML et Css en reproduisant une maquette réalisé par un Web Designer"
+const bookiDescription = "Construction d'un site web d'hébergement avec de simple language HTML et Css en reproduisant une maquette réalisé par un Web Designer.<br> Ma difficulté sur ce projet à était la disposition des éléments mais un bon cour sur la Flex-box avec CSS m'a grandement aidé.<br><br><a href='https://github.com/Younes-Charguioui/booki_website_charguioui_younes'>Lien vers le Github</a>"
 const bookiImages = ["./images/booki/accueil.png","./images/booki/bas-page.png","./images/booki/tablette-acceuil.png","./images/booki/tablette-bas-page.png","./images/booki/mobile-accueil.png","./images/booki/mobile-middle.png","./images/booki/mobile-bas-page.png"]
 const bruelTechno = "- HTML<br>- Css<br>- JavaScript<br>- Postman<br>- Git"
-const bruelDescription = "Amélioration d'un site statique en un site dynamique avec un panel de gestion administrateur pour gérer l'ensemble des opérations possible."
+const bruelDescription = "Amélioration d'un site statique en un site dynamique avec un panel de gestion administrateur pour gérer l'ensemble des opérations possible.<br>Ici le point où j'ai passer le plus de temps est la requete HTTP et l'utilisation des données reçu. Je m'en suis sorti en utilisant la console avec JavaScript pour voir les données traité à chaque manipulation.<br><br><a href='https://github.com/Younes-Charguioui/Portfolio-architecte-sophie-bluel-master'>Lien vers le Github</a>"
 const bruelImages = ["./images/sophie-bruel/accueil.png","./images/sophie-bruel/filtre-portfolio.png","./images/sophie-bruel/page-login.png","./images/sophie-bruel/page-login-error.png","./images/sophie-bruel/admin-acceuil.png","./images/sophie-bruel/admin-gestion-portfolio.png"]
 const carducciTechno = "- Google Lighthouse<br>- Wave Accessibility<br>- SEO Schema.org<br>- JavaScript"
 const carducciDescription = "[SEO] Diagnostic d'un site extremement non performant, avant de procéder à une optimisations sur toutes les images, et chargement pouvant bloquer l'affichage. Sans oublié le reférencement et la correction de la structure du code."
 const carducciImages = ["./images/nina-carducci/accueil.png","./images/nina-carducci/portfolio.png","./images/nina-carducci/audit-no-optimized.png","./images/nina-carducci/audit-optimized.png"]
 const kasaTechno = "- React<br>- Sass"
-const kasaDescription = "Réalisation du Front-End d'un site de location d'hébergement avec React et Sass pour obtenir un site dynamique, performant et super ergonomique."
+const kasaDescription = "Réalisation du Front-End d'un site de location d'hébergement avec React et Sass pour obtenir un site dynamique, performant et super ergonomique.<br><br><a href='https://github.com/Younes-Charguioui/kasa-app-charguioui-younes'>Lien vers le Github</a>"
 const kasaImages = ["./images/kasa/accueil.png","./images/kasa/bas-page.png","./images/kasa/item.png","./images/kasa/item-second.png","./images/kasa/apropos.png","./images/kasa/apropos-colapse.png","./images/kasa/mobile-accueil.png","./images/kasa/mobile-item.png","./images/kasa/mobile-apropos.png"]
 const menumakerTechno = "- Feedly<br>- Trello<br>- Word<br>- Powerpoint"
 const menumakerDescription = "Organisation et gestion de la faisabilité d'un projet de développement d'une application web.<br>Mise en place d'une veille informationnelle avec Feedly.<br>Mise en place de la méthode CRUD pour la gestion de projet."
